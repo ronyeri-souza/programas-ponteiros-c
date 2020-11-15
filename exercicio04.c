82c1fe466d373af296ce63e6b1225367e5258392
ronyeri-souza <souzaronny775@gmail.com>
2020-11-15 11:13:49 -0300

    Exercicio 04

---
+++ exercicio04.c
@@ -0,0 +1,53 @@
+#include <conio.h>
+#include <stdio.h>
+
+void programa01() {
+	
+	int vet[] = {4, 9, 13};
+	int i;
+	
+	printf("\nPrograma 01\n");
+	for(i = 0; i < 3; i++) {
+		
+		printf("%d", *(vet+i));
+		
+	}
+	
+	printf("\n");
+}
+
+void programa02() {
+
+	int vet[] = {4, 9, 13};
+	int i;
+	printf("\nPrograma 02 \n");
+	
+	for(i = 0; i < 3; i++) {
+	
+		printf("%x\t", vet + i);
+	}
+	
+	printf("\n");
+}
+
+void programa03() {
+	
+	int vet[] = {4, 9, 13};
+	int i;
+	printf("\nPrograma 03 \n");
+	
+	for(i = 0; i < 3; i++) {
+	
+		printf("%x\t", vet + i);
+	}
+	
+	printf("\n");
+}
+
+int main(int argc, char *argv[]) {
+	
+	programa01();
+	programa02();
+	programa03();
+	return 0;
+}
